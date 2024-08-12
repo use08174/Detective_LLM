95e41d942390a13c8c05493b4a1f85a1a752add8
Gagasbandas <kjjcds@gmail.com>
2024-08-12 19:53:04 +0900

    대화 가능하게

--- grontown/src/rpg/data/characters.ts
+++ grontown/src/rpg/data/characters.ts
@@ -50,42 +50,42 @@ const characters: Record<string, Character> = {
     eastworldId: "David",
     sprite: "police_officer",
     photo: "assets/photos/david.jpeg",
-    interactTopic: Topics.enterArrestModal,
+    interactTopic: Topics.enterChat,
   }),
 
   Kim: createCharacter({
     eastworldId: "Kim",
     sprite: "police_officer",
     photo: "assets/photos/kim.jpg",
-    interactTopic: Topics.enterArrestModal,
+    interactTopic: Topics.enterChat,
   }),
 
   Woohyun: createCharacter({
     eastworldId: "Woohyun",
     sprite: "police_officer",
     photo: "assets/photos/woohyun_jo.jpg",
-    interactTopic: Topics.enterArrestModal,
+    interactTopic: Topics.enterChat,
   }),
 
   Soyeon: createCharacter({
     eastworldId: "Soyeon",
     sprite: "police_officer",
     photo: "assets/photos/soyeon_bae.jpeg",
-    interactTopic: Topics.enterArrestModal,
+    interactTopic: Topics.enterChat,
   }),
 
   Jiyoon: createCharacter({
     eastworldId: "Jiyoon",
     sprite: "police_officer",
     photo: "assets/photos/jiyoon_jeon.jpeg",
-    interactTopic: Topics.enterArrestModal,
+    interactTopic: Topics.enterChat,
   }),
 
   Eunha: createCharacter({
     eastworldId: "Eunha",
     sprite: "police_officer",
     photo: "assets/photos/eunha_seo.jpeg",
-    interactTopic: Topics.enterArrestModal,
+    interactTopic: Topics.enterChat,
   }),
 };
 
